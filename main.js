ec24e94a0b86821bf3cfe18039b05d98a1c8a20e
Cargalher <cargalher@gmail.com>
2021-04-27 12:09:26 +0200

    second snack

---
+++ main.js
@@ -0,0 +1,20 @@
+// L’utente inserisce due parole in successione, con due prompt.
+
+// var word_1 = prompt ('type a word');
+// var word_2= prompt ('type another word');
+
+// console.log(word_1, word_2);
+
+// // Il software stampa prima la parola più corta, poi la parola più lunga.
+// if ()
+
+
+
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti. (edited) 
+var result=[];
+
+for (var i=0; i<10;i++){
+    result.push(prompt ("Enter a number"));
+}
+console.log(result);
