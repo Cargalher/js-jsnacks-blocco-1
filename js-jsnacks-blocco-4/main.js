f29c50d9b77e9ba033c53a640d39651181d4eb5a
Cargalher <cargalher@gmail.com>
2021-05-10 12:10:51 +0200

    values given to keys

--- js-jsnacks-blocco-4/main.js
+++ js-jsnacks-blocco-4/main.js
@@ -2,24 +2,43 @@
 /*Creare un oggetto palla che abbia le seguenti proprietà.
     Nome = palla
     Peso = 10*/
-var palla = {
-    nome : 'pala',
-    peso : 10,
+// var palla = {
+//     nome : 'pala',
+//     peso : 10,
     
-} 
-console.log(pala);
+// } 
+// console.log(pala);
 
 // Snack 4_1b:
 /*Attraverso un prompt dare la possibilità all’utente di modificare il peso della palla.*/
-var pala = [
-    {
-        nome : 'pala',
-        peso : 10,
+// var pala = [
+//     {
+//         nome : 'pala',
+//         peso : 10,
         
-    } 
-]
-palla.push({
-    peso:parseInt(prompt ("change the balls weight: ")),
-   
-})
-console.log(palla);
+//     } 
+// ]
+
+// var questionPeso = Number (prompt ("Insert new weight: "));
+// var newPeso = {'peso':questionPeso};
+// pala.push(newPeso);
+// console.log(pala);
+
+
+/*fabio's way
+var nuovoPeso = parseInt(prompt( 'inserisci il peso della pala:'));
+console.log(nuovoPeso);
+pala.peso = nuovoPeso;
+console.log(palla); */
+
+// Snack 4_2:
+/*
+Creare un oggetto che rappresenti un triangolo rettangolo, con le seguenti proprietà: base e altezza.
+Calcolare perimetro e area.
+(ripassiamo il Teorema di Pitagora ;D ) */
+
+var triangle ={
+    base: 10,
+    height:4
+}
+function perimeter() {
