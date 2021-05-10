b258d6953c6b3f26813a94020e03a52971976c15
Cargalher <cargalher@gmail.com>
2021-05-10 11:47:11 +0200

    second snack done

--- js-jsnacks-blocco-4/main.js
+++ js-jsnacks-blocco-4/main.js
@@ -1,10 +1,25 @@
-// 1a:
-// Creare un oggetto palla che abbia le seguenti proprietà.
-// Nome = palla
-// Peso = 10
+// Snack 4_1a:
+/*Creare un oggetto palla che abbia le seguenti proprietà.
+    Nome = palla
+    Peso = 10*/
 var palla = {
     nome : 'pala',
     peso : 10,
     
 } 
 console.log(pala);
+
+// Snack 4_1b:
+/*Attraverso un prompt dare la possibilità all’utente di modificare il peso della palla.*/
+var pala = [
+    {
+        nome : 'pala',
+        peso : 10,
+        
+    } 
+]
+palla.push({
+    peso:parseInt(prompt ("change the balls weight: ")),
+   
+})
+console.log(palla);
